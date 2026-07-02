d41c521603590ad525c9b22809e5e4f4dc59d9b8
truskoa1 <truskoa1@gmail.com>
2026-07-02 17:13:48 -0400

    Changes Made:
    1. constants.py: I changed the mass-luminosity power index from 4 to 2.
    
    2. analysis.ipynb: I made a new contour plot on an M_L vs D_L grid of the uncertainty in m_L and z in terms of the observables to observe their behavior and analyze special cases.

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ This script contains the constants used in the microlensing analysis.
 '''
 
 kappa = 8.144  # mas/M_sun
-a = 4.0        # power-law value of mass-luminosity relation
+a = 2.0        # power-law value of mass-luminosity relation
 A_b = 0.0     # extinction from galactic dust
 M_0 = 1.0      # reference mass in solar masses
 L_0 = 1.0      # reference luminosity in solar luminosities
